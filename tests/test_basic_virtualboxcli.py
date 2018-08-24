6d94676c56360e9bec18af43ed4418202c8aebfc
jcnaud <jeancharles.naud@gmail.com>
2018-08-24 13:03:43 +0200

    Correction test virtualbox list.

--- tests/test_basic_virtualboxcli.py
+++ tests/test_basic_virtualboxcli.py
@@ -56,10 +56,8 @@ def test_list():
     cmd = PROGRAM+' --list'
     result = run_cmd(cmd, cwd=DIR_PROGRAM)
 
-    print(result)
-
     # Compare result
-    assert json.loads(result)
+    assert isinstance(json.loads(result), list)
 
 
 def test_list_ostype():
