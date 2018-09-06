04d34a14948947dd334d7108c242ecc617ea6835
jcnaud <jeancharles.naud@gmail.com>
2018-09-06 20:54:58 +0200

    Try debug travis pytest-cov

--- tests/test_basic_custom.py
+++ tests/test_basic_custom.py
@@ -14,7 +14,7 @@ except Exception:
     # Local import
     DIR_PWD = os.path.dirname(os.path.realpath(__file__))
     sys.path.append(os.path.join(DIR_PWD, ".."))
-    from linuxiso.custom.core  import Custom  # noqa
+    from linuxiso.custom.core import Custom  # noqa
     from linuxiso.ressources.tools import load_conf  # noqa
 
 requests_mock.mock.case_sensitive = True
